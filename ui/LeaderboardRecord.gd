19f8c7dc0297236b59a2c56c50267ced32290337
David Snopek <dsnopek@gmail.com>
2021-01-01 07:45:38 -0600

    Fixed overlapping leaderboard records in the UI

--- ui/LeaderboardRecord.gd
+++ ui/LeaderboardRecord.gd
@@ -1,5 +1,5 @@
-extends Control
+extends HBoxContainer
 
 func setup(username, score) -> void:
-	$HBoxContainer/UsernameLabel.text = username
-	$HBoxContainer/WinsLabel.text = str(score)
+	$UsernameLabel.text = username
+	$WinsLabel.text = str(score)
